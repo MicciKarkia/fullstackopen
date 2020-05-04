9c38ef2a8c015cad4157e7668c783054ce39a907
Michaela Karkia <michaela@karkia.me>
2020-05-04 17:14:47 +0300

    unicafe step4 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+const Statistics = ({ good, neutral, bad, all, average, positive, feedback }) => {
   return (
     <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {all}</p>
-    <p>average {average}</p>
-    <p>positive {positive} %</p>
+      <h1>statistics</h1>
+      {feedback ? (
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average}</p>
+          <p>positive {positive} %</p>
+        </div>
+      ) : (
+        <p>No feedback given</p> 
+      )}
     </div>
   )
 }
@@ -21,11 +27,13 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [clicks, setClicks] = useState(0)
   const [score, setScore] = useState(0)
+  const [feedback, setFeedback] = useState(false)
   const average = clicks ? score / clicks : '-' 
   const positive = good ? (good / clicks) * 100.0 : '-'
 
   const handleClick = id => () => {
     setClicks(clicks + 1)
+    setFeedback(true)
 
     if (id === 'good') {
         setGood(good + 1)
@@ -45,7 +53,7 @@ const App = () => {
       <button onClick={handleClick('good')}>good</button>
       <button onClick={handleClick('neutral')}>neutral</button>
       <button onClick={handleClick('bad')}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} all={clicks} average={average} positive={positive} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={clicks} average={average} positive={positive} feedback={feedback} />
     </>
   )
 }
