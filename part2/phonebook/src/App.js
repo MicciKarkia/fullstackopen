415f7a8ad74e7436f6f4cc1aea6e39fd8cd73936
Michaela Karkia <michaela@karkia.me>
2020-05-08 14:49:37 +0300

    phonebook step4 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{
-    name: 'Arto Hellas',
-    number: '040-1234567'
-  }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchResult, setSearchResult] = useState(null)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -25,6 +28,12 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleSearch = (event) => {
+    console.log('Searching for:', event.target.value)
+    const searchTerm = event.target.value
+    setSearchResult(persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase())))
+  }
+
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -35,9 +44,31 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const rows = () => {
+    if (searchResult) {
+      if (searchResult.length) {
+        return ( searchResult.map(person =>
+       <li key={person.name}>{person.name} {person.number}</li>)
+      )
+      } else {
+        return ( <li>No matching search results</li>)
+      }
+      
+    } else {
+     return ( persons.map(person =>
+       <li key={person.name}>{person.name} {person.number}</li>)
+     )
+    }
+      
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input onChange={handleSearch} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -55,9 +86,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul style={{listStyle:'none', padding: 0}}>
-        {persons.map(person =>
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
+        {rows()}
       </ul>
     </div>
   )
