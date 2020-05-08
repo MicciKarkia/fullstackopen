71bdde3d9c59c8a85bb3b154626422bd5c907964
Michaela Karkia <michaela@karkia.me>
2020-05-08 14:29:26 +0300

    phonebook step3 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([{
-    name: 'Arto Hellas'
+    name: 'Arto Hellas',
+    number: '040-1234567'
   }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber
     }
  
     const isFound = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
@@ -19,12 +22,17 @@ const App = () => {
     isFound ? showPopup() : addPersonObject()
     
     setNewName('')
-
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -37,13 +45,18 @@ const App = () => {
             onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul style={{listStyle:'none', padding: 0}}>
         {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
