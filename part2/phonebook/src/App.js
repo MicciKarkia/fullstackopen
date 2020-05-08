ad6be8de0c88d23b35e939ff63ca841d55ba2056
Michaela Karkia <michaela@karkia.me>
2020-05-08 14:15:45 +0300

    phonebook step2 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,9 +12,7 @@ const App = () => {
       name: newName,
     }
  
-    const sameName = () => persons.filter(person => person.name.toLowerCase() === newName.toLowerCase())
-    const isFound = sameName().length
-    console.log(sameName())
+    const isFound = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
     const showPopup = () => window.alert(`No duplicates! \n${newName} is already added to phonebook`)
     const addPersonObject = () => setPersons(persons.concat(personObject))
 
